1019c7a8f3a76e9278c4410acca5355e8acf3a87
Marouane <184505120+marj-ayy@users.noreply.github.com>
2025-04-24 17:23:07 +0200

    Add tab switching functionality

--- src/index.js
+++ src/index.js
@@ -2,3 +2,23 @@ import home from "./home.js";
 import menu from "./menu.js";
 import contact from "./contact.js";
 home();
+const buttons = document.querySelectorAll("button");
+const content = document.querySelector("#content");
+for(const button of buttons){
+    button.addEventListener("click", (e) => {
+        if(e.target.textContent === "Home"){
+            content.innerHTML = "";
+            home();
+        }
+        else if(e.target.textContent === "Menu"){
+            content.innerHTML = "";
+            menu();
+        }
+        else{
+            content.innerHTML = "";
+            contact();
+        }
+    });
+}
+
+
