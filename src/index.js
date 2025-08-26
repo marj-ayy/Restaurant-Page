b986761002979cc78a78aa6a23d70a0a4dbf7a23
Marouane <184505120+marj-ayy@users.noreply.github.com>
2025-08-26 17:59:19 +0200

    Finish header styling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import home from "./home.js";
 import menu from "./menu.js";
 import contact from "./contact.js";
+import "./styles.css";
+
 home();
 const buttons = document.querySelectorAll("button");
 const content = document.querySelector("#content");
 for(const button of buttons){
     button.addEventListener("click", (e) => {
+        for (let loopButton of buttons) {
+            if (loopButton.textContent !== e.target.textContent) {
+                loopButton.style.backgroundColor = "#ffde73";
+            }
+            else {
+                loopButton.style.backgroundColor = "rgb(236,164,59)";
+            }
+        }
         if(e.target.textContent === "Home"){
             content.innerHTML = "";
             home();
